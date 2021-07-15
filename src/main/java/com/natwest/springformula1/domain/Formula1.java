8d33c3709bc531fda4f8f7e6d6445fa734f955e4
d4nmc <danielmcgee.work@gmail.com>
2021-07-15 16:43:10 +0100

    fixed FU1

--- src/main/java/com/natwest/springformula1/domain/Formula1.java
+++ src/main/java/com/natwest/springformula1/domain/Formula1.java
@@ -1,60 +1,61 @@
 package com.natwest.springformula1.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+
+@Entity
 public class Formula1 {
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private String trackName;
 	private String country;
 	private double trackLength;
 	private boolean streetTrack;
 	
 	public Formula1() {}
-	
-	@Entity
-		
-	public FormulaOne(String trackName, String country, double trackLength, boolean streetTrack) {
+			
+	public Formula1(String trackName, String country, double trackLength, boolean streetTrack) {
 		this.trackName = trackName;
 		this.country = country;
 		this.trackLength = trackLength;
 		this.streetTrack = streetTrack;
 	}
-	
+
 	public String getTrackName() {
 		return trackName;
 	}
-	
+
 	public void setTrackName(String trackName) {
 		this.trackName = trackName;
 	}
+
 	public String getCountry() {
 		return country;
 	}
+
 	public void setCountry(String country) {
 		this.country = country;
 	}
+
 	public double getTrackLength() {
 		return trackLength;
 	}
-	public void setLength(double length) {
-		this.trackLength = length;
+
+	public void setTrackLength(double trackLength) {
+		this.trackLength = trackLength;
 	}
+
 	public boolean isStreetTrack() {
 		return streetTrack;
 	}
+
 	public void setStreetTrack(boolean streetTrack) {
 		this.streetTrack = streetTrack;
 	}
-	
-	public Formula1(String trackName, String country, double length, boolean streetTrack) {
-		super();
-		this.trackName = trackName;
-		this.country = country;
-		this.trackLength = length;
-		this.streetTrack = streetTrack;
-	}
-	
-	
-	
+
+
 	
 }
